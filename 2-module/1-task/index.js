e75420757a7b8b2f53c522949e92731a645c98ca
kernelidze <kernelidze@gmail.com>
2022-01-25 13:56:31 +0200

    Второй модуль

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary(value) {
+  let sum = 0;
+  for (let key in value) {
+    if (!isNaN(value[key]) && value[key] !== false && value[key] !== true && value[key] !== Infinity && value[key] !== -Infinity) {
+      sum += value[key];
+    }
+  }
+  return sum;
 }
