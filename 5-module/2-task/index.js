fcf9fd141b0a32cf40195570123c702a2a386bb4
kernelidze <kernelidze@gmail.com>
2022-02-10 19:30:51 +0200

    Задание 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  const toggleButton = document.querySelector('.toggle-text-button');
+  const text = document.querySelector('#text');
+  toggleButton.addEventListener('click', () => {
+    text.toggleAttribute('hidden');
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+  const rightButton = document.querySelector('.carousel__arrow_right');
+  const leftButton = document.querySelector('.carousel__arrow_left');
+  const slides = document.querySelector('.carousel__inner');
+  const image = document.querySelector('.carousel__img');
+  let positionCount = 0;
+  let translateCount = 0;
+  let imageWidth = image.offsetWidth;
+  leftButton.style.display = 'none';
+
+  rightButton.addEventListener('click', () => {
+    positionCount += 1;
+    translateCount += -imageWidth;
+    rightButton.style.display = '';
+    leftButton.style.display = '';
+
+    if (positionCount === 3) {
+      rightButton.style.display = 'none';
+    }
+
+    slides.style.transform = `translateX(${translateCount}px)`;
+  });
+
+  leftButton.addEventListener('click', () => {
+    positionCount -= 1;
+    translateCount += imageWidth;
+    rightButton.style.display = '';
+    leftButton.style.display = '';
+
+    if (positionCount === 0) {
+      leftButton.style.display = 'none';
+    }
+
+    slides.style.transform = `translateX(${translateCount}px)`;
+  });
 }
