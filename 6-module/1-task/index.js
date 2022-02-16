a789f158e64dd1c88351a16f1530e91b18a707ef
kernelidze <kernelidze@gmail.com>
2022-02-16 18:59:28 +0200

    Шестой модуль

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,47 @@
  *      }
  *
  */
+
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+
+    const myTable = document.createElement('table');
+    myTable.innerHTML = this.tableTemplate();
+    this.elem = myTable;
+
+    const tableRows = myTable.querySelectorAll('tr');
+
+    for (let i = 1; i < tableRows.length; i++) {
+      tableRows[i].lastElementChild.onclick = () => {
+        tableRows[i].remove();
+      };
+    }
+  }
+
+  tableTemplate() {
+    return `
+    <table>
+      <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+      </thead>
+      <tbody>
+      ${this.rows.map(row => `<tr>
+            <td>${row.name}</td>
+            <td>${row.age}</td>
+            <td>${row.salary}</td>
+            <td>${row.city}</td>
+            <td><button>X</button></td>
+        </tr>`).join('')
+      }
+      </tbody>
+    </table>
+    `;
   }
 }
